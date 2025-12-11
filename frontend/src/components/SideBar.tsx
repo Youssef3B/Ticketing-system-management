87f9a545bc81413c20ce5efa902d73d08e06146f
Youssef Ababou <3b.youssef@gmail.com>
2025-12-11 01:19:01 +0100

    v1 of sideBar

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,5 +1,45 @@
+import {
+  IconBrandTabler,
+  IconLogout2,
+  IconTicket,
+  IconUsers,
+  IconSettings,
+} from "@tabler/icons-react";
+
 function SideBar() {
-  return <div className="bg-red-500 h-screen w-80 sticky left-0">SideBar</div>;
+  return (
+    <div className="bg-gray-900 h-screen w-80 sticky left-0 p-6 flex flex-col">
+      <h2 className="text-4xl text-white font-bold mb-9">
+        Ticket<span className="text-primary">Hub</span>
+      </h2>
+
+      <ul>
+        <li className="flex items-center space-x-2 bg-primary p-2 rounded-sm text-white font-semibold cursor-pointer my-6">
+          <IconBrandTabler stroke={2} />
+          <span>Dashboard</span>
+        </li>
+        <li className="flex items-center space-x-2 p-2 rounded-sm text-white font-semibold cursor-pointer my-6">
+          <IconTicket stroke={2} />
+          <span>All Tickets</span>
+        </li>
+        <li className="flex items-center space-x-2 p-2 rounded-sm text-white font-semibold cursor-pointer my-6">
+          <IconUsers stroke={2} />
+          <span>All Users</span>
+        </li>
+        <li className="flex items-center space-x-2 p-2 rounded-sm text-white font-semibold cursor-pointer my-6">
+          <IconSettings stroke={2} />
+          <span>Settings</span>
+        </li>
+      </ul>
+
+      <ul className="mt-auto">
+        <li className="flex items-center space-x-2  p-2 rounded-sm text-white font-semibold cursor-pointer my-6">
+          <IconLogout2 stroke={2} />
+          <span>Sign Out</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default SideBar;
